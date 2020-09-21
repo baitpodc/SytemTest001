8e7a7110759b6abc745f6864c0323160fbf7ca6f
17033 <17033@192.168.1.40>
2020-09-21 19:59:08 -0400

    Commit 9212020

--- src/test/java/GitProject/Git_Eclipse/Test001.java
+++ src/test/java/GitProject/Git_Eclipse/Test001.java
@@ -6,6 +6,7 @@ public class Test001 {
 		// TODO Auto-generated method stub
 System.out.println("THis is test");
 System.out.println("THis is test 2");
+System.out.println("THis is test 3");
 
 	}
 
