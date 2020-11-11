c335402189d236981f8e3604451879ab8f7b12af
17033 <17033@192.168.1.40>
2020-11-10 19:29:53 -0500

    First Commit

--- src/test/java/GitProject/Git_Eclipse/Test001.java
+++ src/test/java/GitProject/Git_Eclipse/Test001.java
@@ -5,12 +5,11 @@ public class Test001 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("THis is test");
-System.out.println("THis is test 2");
-System.out.println("THis is test 3");
-System.out.println("THis is test 4");
-System.out.println("THis is test 5");
-System.out.println("THis is test 6");
-System.out.println("THis is test 7");
+
 	}
 
+	
+
+	
+	
 }
